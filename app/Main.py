8ef8d53a2a2bdb7fa9c1c0092f26076174b1433f
Abhishek Gorti <abhishek.gorti@gmail.com>
2016-03-03 20:34:23 -0500

    Changed index

--- app/Main.py
+++ app/Main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask (default)"
+    return "Hello TEST World from Flask (default)"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
